c6744fc8b2629c1acfd24fbce6e9c8310f950b62
denissebr <joziitha642@gmail.com>
2019-08-11 04:00:55 -0600

    Estructura de la puntuacion

---
+++ puntuacion.py
@@ -0,0 +1,32 @@
+class nodopuntuacion:
+    def __init__(self, posx, posy):
+        self.posicionx = posx
+        self.posiciony = posy
+        self.anterior = None
+
+class pilapuntuacion:
+    def __init__(self):
+        self.pila = None
+
+    def push(self, posx, posy):
+        nuevonodo = nodopuntuacion(posx, posy)
+        if self.pila is None:
+            self.pila = nuevonodo
+        else:
+            nuevonodo.anterior = self.pila
+            self.pila = nuevonodo
+
+    def pop(self):
+        nodoaux = self.pila
+        self.pila = self.pila.anterior
+        nodoaux.anterior = None
+
+    def size(self):
+        if self.pila is None:
+            return 0
+        else:
+            nodoaux = self.pila
+            contador = 1
+            while nodoaux:
+                nodoaux = nodoaux.anterior
+                contador += 1
