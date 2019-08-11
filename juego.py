3470490f44f30de2aaa2ad701fd8df5d0ad51fc9
denissebr <joziitha642@gmail.com>
2019-08-11 04:06:52 -0600

    Mostrar la puntuacion dependiendo del tamaño de la pila

--- juego.py
+++ juego.py
@@ -1,5 +1,6 @@
 import curses
 import serpiente
+import puntuacion
 from curses import KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN
 
 class juego:
@@ -7,15 +8,17 @@ class juego:
         self.window = window
         self.window.clear()
         self.window.border(0)
-        self.window.addstr(0, 0, "Puntuacion: 0")
         self.posiniciousuario = 100 - len(usuario)
         self.usuario = usuario
         self.window.addstr(0, self.posiniciousuario - 2, self.usuario)
         snake = serpiente.cuerposerpiente(10, 15, window, timeout)
+        score = puntuacion.pilapuntuacion()
+        self.window.addstr(0, 0, "Puntuacion: {}".format(score.size()))
         self.window.timeout(timeout)
         while True:
             self.window.clear()
             self.window.border(0)
+            self.window.addstr(0, 0, "Puntuacion: {}".format(score.size()))
             snake.pintar()
             self.window.addstr(34, 0, "Presione P para pausar")
             self.window.addstr(0, self.posiniciousuario - 2, self.usuario)
